6dd365aaa44089779fb6716ace995c9f7a2018f0
Chengfffff <una06021209@gmail.com>
2026-03-12 13:16:30 +0800

    Coupon功能完成

---
+++ EatTogether/Models/Extensions/CouponDtoExtensions.cs
@@ -0,0 +1,26 @@
+﻿using EatTogether.Models.DTOs;
+using EatTogether.Models.EfModels;
+
+namespace EatTogether.Models.Extensions
+{
+    public static class CouponDtoExtensions
+    {
+        // EFModel → DTO
+        public static CouponDto ToDto(this Coupon c)
+        {
+            return new CouponDto
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Code = c.Code,
+                DiscountType = c.DiscountType,
+                DiscountValue = c.DiscountValue,
+                MinSpend = c.MinSpend,
+                StartDate = c.StartDate,
+                EndDate = c.EndDate,
+                LimitCount = c.LimitCount,
+                ReceivedCount = c.ReceivedCount ?? 0
+            };
+        }
+    }
+}
---
+++ EatTogether/Models/Repositories/ICouponRepository.cs
@@ -0,0 +1,23 @@
+﻿using EatTogether.Models.DTOs;
+
+namespace EatTogether.Models.Repositories
+{
+    public interface ICouponRepository
+    {
+        Task<IEnumerable<CouponDto>> GetAllAsync();
+        Task<CouponDto?> GetByIdAsync(int id);
+        Task<CouponDto?> GetByCodeAsync(string code);
+        Task CreateAsync(CouponDto dto);
+        Task<bool> IsCodeExistsAsync(string code);
+        Task IncrementReceivedCountAsync(int id);
+    }
+
+    public interface IMemberCouponRepository
+    {
+        Task<IEnumerable<MemberCouponDto>> GetAllAsync();
+        Task<MemberCouponDto?> GetByMemberAndCouponAsync(int memberId, int couponId);
+        Task<IEnumerable<MemberCouponDto>> GetByMemberAsync(int memberId);
+        Task AddAsync(int memberId, int couponId);
+        Task MarkAsUsedAsync(int memberId, int couponId);
+    }
+}
