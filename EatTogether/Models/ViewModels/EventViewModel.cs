9b2dddb6c6925ce740b737ee5e33f7d7095fc5f4
Weiyan-99 <g2301149040@gmail.com>
2026-03-13 11:19:57 +0800

    Events Index view美化

--- EatTogether/Models/ViewModels/EventViewModel.cs
+++ EatTogether/Models/ViewModels/EventViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EatTogether.Models.ViewModels
 {
@@ -16,9 +17,11 @@ namespace EatTogether.Models.ViewModels
 		public int MinSpend { get; set; }
 
 		[Display(Name = "開始日期")]
+		[DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
 		public DateTime StartDate { get; set; }
 
 		[Display(Name = "結束日期")]
+		[DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
 		public DateTime EndDate { get; set; }
 
 		[Display(Name = "贈品")]
@@ -28,6 +31,7 @@ namespace EatTogether.Models.ViewModels
 		public string? DiscountType { get; set; }
 
 		[Display(Name = "折扣金額")]
+		[DisplayFormat(DataFormatString = "{0:0}", ApplyFormatInEditMode = false)]
 		public decimal DiscountValue { get; set; }
 
 		[Display(Name = "狀態")]
